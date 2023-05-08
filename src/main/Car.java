725565239e185e6a9987dea7d7471fc3e0b3a803
Ceyhun <ceyhunqurbanzade@gmail.com>
2023-05-08 15:48:03 +0400

    Initial commit

---
+++ src/main/Car.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Car {
+    public int a =2;
+    public void drive(){
+        System.out.println("Car drive");
+    }
+
+}
---
+++ src/main/main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+       BMW b = new BMW();
+       b.drive();
+
+
+        Car c = b;
+        c.drive();
+        
+
+
+
+
+    }
+
+}
